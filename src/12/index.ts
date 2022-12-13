d068ca4a7b10892f064bd1cdd78c7c67d1cc8caf
vladshlianin <vladshlyanin@gmail.com>
2022-12-13 19:16:45 +0200

    Solving day 13

--- src/12/index.ts
+++ src/12/index.ts
@@ -1,10 +1,8 @@
 import path from 'path';
 import { calculateStepsPart1, calculateStepsPart2 } from './calculateSteps';
 
-// const INPUT_PATH = path.join(__dirname, '../../tests/12/input.txt');
 const INPUT_PATH = path.join(__dirname, 'input.txt');
 const result1 = calculateStepsPart1(INPUT_PATH);
-// Part 2 is VERY long
 const result2 = calculateStepsPart2(INPUT_PATH);
 
 console.log(`Result for part 1 is ${result1}`);
